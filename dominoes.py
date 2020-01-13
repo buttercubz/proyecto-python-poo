4315a653cecb830b0056a4263e84f76ca74ca140
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-01-13 14:09:06 -0400

    agredado estructura de datos y algunos metodo

--- dominoes.py
+++ dominoes.py
@@ -2,7 +2,7 @@ class Domino():
     def __init__ (self, dominoFace1, dominoFace2):
         self.dominoFace1 = dominoFace1 #la parte izquierda de una ficha
         self.dominoFace2 = dominoFace2 #la parte derecha de la ficha
-        self.dominoFace = str(dominoFace1) + " | " + str(dominoFace2) #la ficha completa
+        self.dominoFace = str(dominoFace1) + " | " + str(dominoFace2) + '|' #la ficha completa
         self.value = dominoFace1 + dominoFace2 #el valor de la ficha
 
 all_Dominoes = [] #Todas las fichas
--- mesa.py
+++ mesa.py
@@ -1,19 +1,17 @@
 from dominoes import all_Dominoes
+import os
 class Table:
     #   tokens son las fichas de domino
     def __init__(self, tokens):
         self.tokens = tokens
         self.empty = []
-        self.cache = ''
+        self.cache = []
+        self.data = None
 #   show dominoes desplega todos los dominos en la mesa
-    def show_dominoes(self, test = False):
-        if test:
-            for i in self.tokens:
-                print(i[0])
-        else:
+    def show_dominoes(self, test = False):#el parametro test es para desplegar todo el domino
             for i in self.empty:
-                for j in i:
-                    self.cache += j
+                self.cache.append(i[0])
+                self.data = ' '.join(self.cache)
             print(self.cache)
     def append_tokens(self, tokens_per_players, place):
         if place == 2:
@@ -24,13 +22,13 @@ class Table:
             return self.empty
         print(self.empty)
 
-
-# print(all_Dominoes)
-
-
-
 nueva_mesa = Table(all_Dominoes)
 while True:
-    nueva_mesa.append_tokens([input('fichas: ')], int(input('choose your place: ')))
-    nueva_mesa.show_dominoes(True)
+    nueva_mesa.append_tokens([input('choose your dominoes: ')], int(input('choose your place: ')))
+    nueva_mesa.show_dominoes()
 
+# a=['hola' , 'estoy' , 'haciendo' , 'un' , 'programa']
+# cadena = " ".join(a)
+
+
+# print(cadena)
