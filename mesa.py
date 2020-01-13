3505e2f64eb33ca3884b89ac24d8f97bd1a32a21
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-01-13 10:47:19 -0400

    se agrego el metodo show dominoes

--- mesa.py
+++ mesa.py
@@ -1,2 +1,9 @@
-class Mesa:
-    
+class Table:
+#   tokens son las fichas de domino
+    def __init__(self, tokens):
+        self.tokens = tokens
+#   show dominoes desplega todos los dominos en la mesa
+    def show_dominoes(self):
+        for i in self.tokens:
+            print(i[0])
+
