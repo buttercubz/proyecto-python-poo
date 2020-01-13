fce5e628139e63a4a37a12678869a3b31eaec829
RivierG <rgrullon@ipisdosaj.edu.do>
2020-01-13 15:46:41 -0400

    add features to human player

--- Player.py
+++ Player.py
@@ -7,13 +7,13 @@ class Player(object):
         
     def take_hand(self,tokens,tokens_per_player = 7): #tomas las fichas para la mano del jugador
         for _ in range(tokens_per_player):
-            self.hand.append(all_Dominoes.pop())
+            self.hand.append(tokens.pop())
         return self
     
     def show_hand(self): #muestra la mano del jugador 
         print(self.hand)
 
-        
+         
 player = Player(input('introduce your name here: ')) 
 player.take_hand(all_Dominoes)
 player.show_hand()
