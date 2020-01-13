983d5ababc43687130a018b8a972bcb7227bed93
Gil <ghaerdi@ipisdosaj.edu.do>
2020-01-13 17:32:06 -0400

    Ahorrar algunas lineas de codigo

--- dominoes.py
+++ dominoes.py
@@ -1,42 +1,22 @@
 import random
 
-class Domino():
+class Domino(): #Clase para crear una pieza
     def __init__ (self, dominoFace1, dominoFace2):
         self.dominoFace1 = dominoFace1 # La parte izquierda de una ficha
         self.dominoFace2 = dominoFace2 # La parte derecha de la ficha
         self.dominoFace = str(dominoFace1) + "|" + str(dominoFace2) # La ficha completa
 
-all_Dominoes = [] # Todas las fichas
+all_Dominoes = [] # Aquí se guardan todas las fichas
 
-
-for i in range(7):
+# Bucle que se encarga de rellenar el arreglo vacio.
+for i in range(1, 6):
     for l in range(7):
         c = Domino(i, l)
         all_Dominoes.append(c.dominoFace)
 
-# Remover las piezas repetidas, ignoren el force v:
-all_Dominoes.remove("1|0")
-all_Dominoes.remove("2|0")
-all_Dominoes.remove("3|0")
-all_Dominoes.remove("4|0")
-all_Dominoes.remove("5|0")
-all_Dominoes.remove("6|0")
-all_Dominoes.remove("2|1")
-all_Dominoes.remove("3|1")
-all_Dominoes.remove("4|1")
-all_Dominoes.remove("5|1")
-all_Dominoes.remove("6|1")
-all_Dominoes.remove("3|2")
-all_Dominoes.remove("4|2")
-all_Dominoes.remove("5|2")
-all_Dominoes.remove("6|2")
-all_Dominoes.remove("4|3")
-all_Dominoes.remove("5|3")
-all_Dominoes.remove("6|3")
-all_Dominoes.remove("4|5")
-all_Dominoes.remove("4|6")
-all_Dominoes.remove("5|6")
+# Remueve las piezas repetidas, ignoren el force v:
+del all_Dominoes[2:6]
+del all_Dominoes[6:9]
 
+# Baraja las fichas
 random.shuffle(all_Dominoes)
-
-print(all_Dominoes)
