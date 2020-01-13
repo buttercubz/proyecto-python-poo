f380707afe8050336c27e4430fd0ecca643efe2f
slorard <slora@ipisdosaj.edu.do>
2020-01-13 10:59:08 -0400

    bots

---
+++ bots.py
@@ -0,0 +1,9 @@
+class bots(object):
+    def __int__(self, fichasHand, name)
+        self.fichasHand = []
+        self.name = str(name)
+    def takeFicha(self, token):
+    def game(self, fichasHand):
+    def show(self, fichasHand):
+    def takeFichaTable(self, fichasHand)
+    
