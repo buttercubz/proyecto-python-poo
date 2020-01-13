7bc1fa3cf52ee342768058911f6fe88b4df79b21
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-01-13 14:37:59 -0400

    nuevos metodos de prueva

--- dominoes.py
+++ dominoes.py
@@ -2,7 +2,7 @@ class Domino():
     def __init__ (self, dominoFace1, dominoFace2):
         self.dominoFace1 = dominoFace1 #la parte izquierda de una ficha
         self.dominoFace2 = dominoFace2 #la parte derecha de la ficha
-        self.dominoFace = str(dominoFace1) + " | " + str(dominoFace2) + '|' #la ficha completa
+        self.dominoFace = str(dominoFace1) + "|" + str(dominoFace2) #la ficha completa
         self.value = dominoFace1 + dominoFace2 #el valor de la ficha
 
 all_Dominoes = [] #Todas las fichas
@@ -10,4 +10,4 @@ all_Dominoes = [] #Todas las fichas
 for i in range(7):
     for l in range(7):
         c = Domino(i, l)
-        all_Dominoes.append([c.dominoFace])
+        all_Dominoes.append(c.dominoFace)
--- mesa.py
+++ mesa.py
@@ -5,14 +5,15 @@ class Table:
     def __init__(self, tokens):
         self.tokens = tokens
         self.empty = []
-        self.cache = []
+        self.cache = ' '
         self.data = None
 #   show dominoes desplega todos los dominos en la mesa
     def show_dominoes(self, test = False):#el parametro test es para desplegar todo el domino
             for i in self.empty:
-                self.cache.append(i[0])
-                self.data = ' '.join(self.cache)
-            print(self.cache)
+                self.data = self.cache.join(self.empty)
+            print(self.data)
+    def get_hand(self):
+        return type(self.data)
     def append_tokens(self, tokens_per_players, place):
         if place == 2:
             self.empty.append(tokens_per_players)
@@ -21,11 +22,16 @@ class Table:
             self.empty.insert(0, tokens_per_players)
             return self.empty
         print(self.empty)
+    def get_points(self, hand):
+        first_step = hand.replace("-", "+")
+        return first_step
 
 nueva_mesa = Table(all_Dominoes)
 while True:
-    nueva_mesa.append_tokens([input('choose your dominoes: ')], int(input('choose your place: ')))
+    nueva_mesa.append_tokens(input('choose your dominoes: '), int(input('choose your place: ')))
+    os.system('clear')
     nueva_mesa.show_dominoes()
+    print(nueva_mesa.get_points(nueva_mesa.get_hand()))
 
 # a=['hola' , 'estoy' , 'haciendo' , 'un' , 'programa']
 # cadena = " ".join(a)
