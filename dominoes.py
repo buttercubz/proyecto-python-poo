bf6e3f451b7ddd6516ef17c08744d258fc5fda84
Gil <ghaerdi@ipisdosaj.edu.do>
2020-01-13 12:35:58 -0400

    Ya escribi condigo claro

--- dominoes.py
+++ dominoes.py
@@ -1,13 +1,13 @@
 class Domino():
     def __init__ (self, dominoFace1, dominoFace2):
-        self.dominoFace1 = dominoFace1
-        self.dominoFace2 = dominoFace2
-        self.dominoFace = str(dominoFace1) + " | " + str(dominoFace2)
-        self.value = dominoFace1 + dominoFace2
-        # self.value = set(self.value)
+        self.dominoFace1 = dominoFace1 #la parte izquierda de una ficha
+        self.dominoFace2 = dominoFace2 #la parte derecha de la ficha
+        self.dominoFace = str(dominoFace1) + " | " + str(dominoFace2) #la ficha completa
+        self.value = dominoFace1 + dominoFace2 #el valor de la ficha
 
-f = []
+All_Dominoes = [] #Todas las fichas
+
 for i in range(7):
     for l in range(7):
         c = Domino(i, l)
-        f.append(c.dominoFace)
+        all_Dominoes.append(c.dominoFace)
